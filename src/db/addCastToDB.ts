65669cbdb15ad5471f42b320e33b952c5ca4449e
Tanishq Sharma <tanishqsh@gmail.com>
2024-05-16 22:06:42 -0400

    new routes, incomplete right now, pushing this to test railway

---
+++ src/db/addCastToDB.ts
@@ -0,0 +1,38 @@
+import { firebase } from '../firebase/firebase';
+
+const addCastToDB = async (cast: any) => {
+	// get the cast has
+	const hash = cast.hash;
+
+	// get the user fid
+	const fid = cast.fid;
+
+	console.log('=============================== ');
+	console.log('Adding cast to the database with following details: ');
+	console.log('Hash: ', hash);
+	console.log('Fid: ', fid);
+	console.log('Cast: ', cast);
+	console.log('=============================== ');
+
+	// prepare the db reference
+
+	const castsRef = firebase.db.collection('casts');
+	const castRef = castsRef.doc(hash);
+
+	console.log('DB reference: ', castRef.path);
+
+	try {
+		// add the data to the db
+		await castRef.set({
+			...cast,
+			lastSyncedAt: firebase.FieldValue.serverTimestamp(),
+			timestamp: firebase.FieldValue.serverTimestamp(),
+		});
+
+		console.log('Cast added to the database successfully');
+	} catch (error) {
+		console.error('Error adding cast to the database:', error);
+	}
+};
+
+export { addCastToDB };
