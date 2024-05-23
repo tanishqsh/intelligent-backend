a3e70ca5cca0e3f9577527b9f261325fdf2cf1a2
Tanishq Sharma <tanishqsh@gmail.com>
2024-05-22 23:25:01 -0400

    Development Redis, Long running AlfaFrens, going to change that

--- src/utils/redisConnection.ts
+++ src/utils/redisConnection.ts
@@ -1,9 +1,46 @@
 import { RedisOptions } from 'ioredis';
+import Redis from 'ioredis';
 
-const connectionOptions: RedisOptions = {
-	host: 'redis-15585.c326.us-east-1-3.ec2.redns.redis-cloud.com',
-	port: 15585,
-	password: 'hbrfbyLJV0Gu5Vid5kr1MGftmEwbZpJr',
+const env = process.env.NODE_ENV || 'development';
+
+type RedisConfig = {
+	host: string | undefined;
+	port: number;
+	password: string | undefined;
 };
+
+const redisConfig: { [key in 'production' | 'development']: RedisConfig } = {
+	production: {
+		host: process.env.REDIS_PRODUCTION_HOST,
+		port: 15585,
+		password: process.env.REDIS_PRODUCTION_PASSWORD,
+	},
+	development: {
+		host: process.env.REDIS_DEVELOPMENT_HOST,
+		port: 15202,
+		password: process.env.REDIS_DEVELOPMENT_PASSWORD,
+	},
+};
+
+const currentConfig: RedisConfig = redisConfig[env as 'production' | 'development'] || redisConfig.development;
+const connectionOptions: RedisOptions = {
+	host: currentConfig.host,
+	port: currentConfig.port,
+	password: currentConfig.password,
+	retryStrategy: (times) => {
+		const delay = Math.min(times * 50, 2000);
+		return delay;
+	},
+};
+
+const redis = new Redis(connectionOptions);
+
+redis.on('connect', () => {
+	console.log('Redis is connected on:', env);
+});
+
+redis.on('error', (err) => {
+	console.error('Redis connection error:', err);
+});
 
 export default connectionOptions;
