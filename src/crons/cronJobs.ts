08963197ea8c0fc62c0489af3314b6c6339337f8
Tanishq Sharma <tanishqsh@gmail.com>
2024-05-23 05:29:02 -0400

    New cron job that syncs the alfafrens every 6 hours

---
+++ src/crons/cronJobs.ts
@@ -0,0 +1,22 @@
+import cron from 'node-cron';
+import fetchUsersFromDB from '../db/fetchUsersFromDB';
+import syncAlfaFrensQueue from '../queues/syncAlfaFrensQueue';
+
+// Schedule a task to run every 6 hours
+cron.schedule('0 */6 * * *', async () => {
+	console.log('Cron job executed every 6 hours');
+	// Add your task logic here
+
+	const users = await fetchUsersFromDB();
+	console.log('Users:', users);
+
+	// add each user to the queue
+	users.forEach((fid) => {
+		addToQueue(fid);
+	});
+});
+
+const addToQueue = async (fid: string) => {
+	const job = await syncAlfaFrensQueue.add(`syncAlfaFrensQueue: ${fid}`, fid);
+	console.log(`Alfa Fren Sync Job for ${fid} added to queue ✅ by cron`);
+};
