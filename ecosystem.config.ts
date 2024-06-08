d4456dcd4081d2ca1b1b2eb117e0f53bb833455e
Tanishq Sharma <tanishqsh@gmail.com>
2024-06-08 15:56:24 -0400

    another attempt to get pm2 ready for heroku

---
+++ ecosystem.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'intelligent-backend',
+      script: 'dist/index.js',
+      watch: true,
+      instances: 'max',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      autorestart: true,  // Ensure the app restarts on crash
+      max_restarts: 10,   // Optional: Limit the number of restarts to avoid infinite loops
+      restart_delay: 5000 // Optional: Delay between restarts (in milliseconds)
+    }
+  ]
+};
